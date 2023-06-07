2158352a123f83d485f7eb25741f527dd7150295
Ilnur <ilnur9456@gmail.com>
2023-06-07 19:52:18 +0400

    "недочеты 2"

--- src/main/java/ru/practicum/shareit/item/dto/ItemWithBooking.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemWithBooking.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.booking.dto.BookingInfo;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
