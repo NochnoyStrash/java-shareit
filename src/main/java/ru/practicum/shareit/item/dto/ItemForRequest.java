bb8151ecf509099e74cbae5332955da9d8735911
Ilnur <ilnur9456@gmail.com>
2023-06-26 04:49:42 +0400

    "добавил реквест 8"

--- src/main/java/ru/practicum/shareit/item/dto/ItemForRequest.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemForRequest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.generrat.Generated;
 
 
